f0664577bc55b9cc092e5b9956c3dd77e30b9612
Omar Abotahoon <oabot040@uottawa.ca>
2024-02-16 11:56:10 -0500

    Added auth and fixed some endpoint syntax

--- server/app.js
+++ server/app.js
@@ -8,17 +8,45 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-//Firestore imports
-const { initializeApp, cert } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
+// //Firestore imports
+// const { initializeApp, cert } = require('firebase-admin/app');
+// const { getFirestore } = require('firebase-admin/firestore');
 
-const serviceAccount = require('./serviceAccountKey.json');
-initializeApp({
-  credential: cert(serviceAccount),
-  databaseURL: process.env.DB_URL,
-});
+// const serviceAccount = require('./serviceAccountKey.json');
+// initializeApp({
+//   credential: cert(serviceAccount),
+//   databaseURL: process.env.DB_URL,
+// });
+
+// const db = getFirestore();
+
+const { initializeApp } = require('firebase/app');
+const { getFirestore } = require('firebase/firestore');
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDQi9MazxIeBVqRC3WKsMmORYgFqWV9kWE',
+  authDomain: 'intellistudy-capstone.firebaseapp.com',
+  projectId: 'intellistudy-capstone',
+  storageBucket: 'intellistudy-capstone.appspot.com',
+  messagingSenderId: '1042357477540',
+  appId: '1:1042357477540:web:4659117a935904c07e0631',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const { getAuth, createUserWithEmailAndPassword } = require('firebase/auth');
+const auth = getAuth();
 
 const db = getFirestore();
+
+const {
+  collection,
+  doc,
+  getDoc,
+  setDoc,
+  addDoc,
+  updateDoc,
+} = require('firebase/firestore');
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
@@ -31,26 +59,33 @@ app.get('/test', (req, res) => {
 // Sample post request to upload data into DB
 app.post('/testDB_post', async (req, res) => {
   console.log(req.body);
-  await db.collection('testCollection').add(req.body);
+  // await db.collection('testCollection').add(req.body);
 
-  res.send({ Message: 'Success',
-            Body: req.body});
+  const testCollectionRef = collection(db, 'testCollection');
+  await addDoc(testCollectionRef, req.body);
+
+  res.send({ Message: 'Success', Body: req.body });
 });
 
 // Sample get request to get data from a certain document
 app.get('/testDB_get', async (req, res) => {
-  const testCollectionRef = db.collection('testCollection').doc('testDoc');
-  const doc = await testCollectionRef.get();
+  // const testCollectionRef = db.collection('testCollection').doc('testDoc');
+  const testCollectionRef = doc(db, 'testCollection', 'testDoc');
+  const docSnap = await getDoc(testCollectionRef);
 
-  console.log(doc.data());
+  console.log(docSnap.data());
 
-  res.send(doc.data());
+  res.send(docSnap.data());
 });
 
 // Sample update request to update data in a certain document without overwriting other fields
 app.put('/testDB_update', async (req, res) => {
   console.log(req.body);
-  await db.collection('testCollection').doc('testDoc').update(req.body);
+
+  const testCollectionRef = doc(db, 'testCollection', 'testDoc');
+  await updateDoc(testCollectionRef, req.body);
+
+  // await db.collection('testCollection').doc('testDoc').update(req.body);
 
   res.send({ Message: 'Success' });
 });
@@ -67,6 +102,24 @@ app.get('/query', async (req, res) => {
 
   // res.send(snapshot.data());
   res.send({ Message: 'Success' });
+});
+
+app.post('/testAuth_create', async (req, res) => {
+  console.log(req.body);
+
+  createUserWithEmailAndPassword(auth, req.body.email, req.body.password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+
+  res.send({ Message: 'Success', Body: req.body });
 });
 
 app.listen(port, () => {
