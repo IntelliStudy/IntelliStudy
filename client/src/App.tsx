cee2068ed93740d9d7139e46bf7beec8701f36d6
omaraly1 <oaly042@uottawa.ca>
2024-03-01 01:28:33 -0500

    Cloud Functions setup plus Auth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,50 +1,29 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+
+import { setDoc, doc } from 'firebase/firestore';
 import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { db } from './firebase/firebase'
+import LoginForm from './components/LoginForm';
+
 
 function App() {
-  const [data, setData] = useState(null);
-  const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get<any>('http://localhost:3000/test');
-        setData(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+  const setValue = ():void => {
 
-    fetchData();
-  }, []); // Empty dependency array to run the effect only once when the component mounts
+    const docRef = doc(db, 'frontendTest', 'eMoZsDt0CZ81ow5M59wf');
+    setDoc(docRef, {generate: false})
+  
+  }
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>IntelliStudy</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={setValue}>
+          Click me test
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <LoginForm/>
       </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      {data}
     </>
   );
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,13 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+export { testTrigger } from './testTrigger';
+
+// export FIREBASE_AUTH_EMULATOR_HOST="127.0.0.1:9099"
+// export FIRESTORE_EMULATOR_HOST="127.0.0.1:8080"
