021b98e35fd3075066e9891992c28c657cce9aeb
Omar Abotahoon <oabot040@uottawa.ca>
2024-09-18 11:53:47 -0400

    Renamed folder

--- client/src/pages/Quiz.tsx
+++ client/src/pages/Quiz.tsx
@@ -8,7 +8,7 @@ import { SectionWrapper } from "../components";
 import { QuestionLabel, QuestionType } from "../constants";
 import { db } from "../firebase/firebase";
 import { QuizFormValues, QuizValidationAnswers } from "../types/quiz";
-import { createAttemptDocument } from "../utilities/quizUtilities";
+import { createAttemptDocument } from "../utilitiesFun/quizUtilities";
 
 const Quiz = () => {
   const { currentUser } = useContext(UserContext);
